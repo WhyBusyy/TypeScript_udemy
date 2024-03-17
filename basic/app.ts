66c5dac7ae17bd2b186967648b4df2ccf1a7f401
WhyBusyy <ybg6152@naver.com>
2024-03-17 19:04:21 +0900

    tuple 타입

--- basic/app.ts
+++ basic/app.ts
@@ -2,11 +2,25 @@
 //   name: string;
 //   age: number;
 // } ={
-const person = {
-  name: "ybg",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-};
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} =
+  // const person
+  {
+    name: "ybg",
+    age: 30,
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"],
+    // TS 튜플 타입
+  };
+
+// * TS 튜플 타입 => push는 예외로 허용됨 *
+person.role.push("admin");
+// person.role[1] = 10;
+// person.role = [0, "admin", "user"];
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
