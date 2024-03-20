1e291a93882cfa3a751dd94324735d40db057771
WhyBusyy <ybg6152@naver.com>
2024-03-20 12:16:01 +0900

    enum 타입

--- basic/app.js
+++ basic/app.js
@@ -1,21 +1,44 @@
 // const person: {
 //   name: string;
 //   age: number;
-// } ={
-var person = 
-// const person
-{
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "ybg",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person = {
     name: "ybg",
     age: 30,
     hobbies: ["Sports", "Cooking"],
-    role: [2, "author"],
+    role: Role.ADMIN,
 };
-person.role.push("admin");
+// * TS 튜플 타입 => push는 예외로 허용됨 *
+// person.role.push("admin");
 // person.role[1] = 10;
+// person.role = [0, "admin", "user"];
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+}
+if (person.role === Role.AUTHOR) {
+    console.log("is author");
 }
--- basic/app.ts
+++ basic/app.ts
@@ -1,24 +1,38 @@
 // const person: {
 //   name: string;
 //   age: number;
-// } ={
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} =
-  // const person
-  {
-    name: "ybg",
-    age: 30,
-    hobbies: ["Sports", "Cooking"],
-    role: [2, "author"],
-    // TS 튜플 타입
-  };
+// } = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "ybg",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: "ybg",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
 
 // * TS 튜플 타입 => push는 예외로 허용됨 *
-person.role.push("admin");
+// person.role.push("admin");
 // person.role[1] = 10;
 // person.role = [0, "admin", "user"];
 
@@ -29,4 +43,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("is author");
 }
