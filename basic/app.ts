199f836756ed0ee8faa37feef9b17e50e84a76f4
WhyBusyy <ybg6152@naver.com>
2024-03-27 13:58:06 +0900

    함수 반환 타입, void, 콜백

--- basic/app.ts
+++ basic/app.ts
@@ -1,47 +1,27 @@
-type Combinable = number | string;
-type ConversionDiscriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultType: ConversionDiscriptor
-) {
-  let result;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultType === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-
-  //   if (resultType === "as-number") {
-  //     return +result;
-  //   } else {
-  //     return result.toString();
-  //   }
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-
-const combineNames = combine("Max", "Anna", "as-text");
-console.log(combineNames);
-
-//----------
-// 객체 타입 alias
-type User = { name: string; age: number };
-const u1: User = { name: "Max", age: 30 }; // this works!
-
-function greet(user: User) {
-  console.log("Hi, I am " + user.name);
+function printResult(num: number) {
+  console.log("Result: " + num);
 }
 
-function isOlder(user: User, checkAge: number) {
-  return checkAge > user.age;
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
 }
+
+printResult(add(5, 12));
+
+// let someValue: undefined;
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+// combineValues = printResult;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
