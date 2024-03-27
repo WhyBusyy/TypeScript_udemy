2dbf9afdce72e5f28b28b4199727d0e7ffb83c29
WhyBusyy <ybg6152@naver.com>
2024-03-27 14:26:36 +0900

    unknown, never

--- basic/app.js
+++ basic/app.js
@@ -1,20 +1,11 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+if (typeof userInput === "string")
+    userName = userInput;
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-function printResult(num) {
-    console.log("Result: " + num);
-}
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-printResult(add(5, 12));
-// let someValue: undefined;
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
-addAndHandle(10, 20, function (result) {
-    console.log(result);
-});
+var result = generateError("An error occurred!", 500);
+console.log(result);
