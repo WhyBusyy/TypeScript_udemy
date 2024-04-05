9b17df0a2fe0b26d6732a169da539661b6bc7833
WhyBusyy <ybg6152@naver.com>
2024-04-05 20:11:20 +0900

    --watch, -w tsc --init

--- basic/app.js
+++ basic/app.js
@@ -1,11 +1,12 @@
-var userInput;
-var userName;
+"use strict";
+let userInput;
+let userName;
 userInput = 5;
 userInput = "Max";
 if (typeof userInput === "string")
     userName = userInput;
 function generateError(message, code) {
     throw { message: message, errorCode: code };
+    // while (true) {}
 }
-var result = generateError("An error occurred!", 500);
-console.log(result);
+generateError("An error occurred!", 500);
