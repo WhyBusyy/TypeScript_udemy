d601ace521b3f9dfc49c6f25b456cf7a5b338c86
WhyBusyy <ybg6152@naver.com>
2024-04-09 18:36:25 +0900

    target, lib 옵션

--- basic/app.js
+++ basic/app.js
@@ -1,12 +1,5 @@
 "use strict";
-let userInput;
-let userName;
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string")
-    userName = userInput;
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-    // while (true) {}
-}
-generateError("An error occurred!", 500);
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    console.log("Clicked");
+});
